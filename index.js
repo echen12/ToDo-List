d6611cbbbbe529818fbef4f99148898cc8f304ec
Edmond <chen.edmond13@gmail.com>
2021-06-10 11:41:11 -0700

    updated

--- index.js
+++ index.js
@@ -4,69 +4,94 @@ app.use(express.json())
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('build'))
+const mongoose = require('mongoose')
 
-let projects = [
-    {
-        projectName: "first project",
-        id: 1,
-        firstdetail: "first detail"
-    },
-    {
-        projectName: "second project",
-        id: 2,
-        seconddetail: "second detail"
-    },
-    {
-        projectName: "third project",
-        id: 3,
-        thirddetail: "third detail"
-    }
-]
+const url = `mongodb+srv://projectList:project@cluster0.nt0wp.mongodb.net/projectList?retryWrites=true&w=majority`
 
-app.get('/api/projects', (request, response) => {
-    response.json(projects)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+const projectSchema = new mongoose.Schema({
+    projectName: String,
+    id: Number,
+    projects: [
+        { task: String, _id: false }
+    ],
 })
 
-app.get('/api/projects/:id', (request, response) => {
-    const id = request.params.id
-    const project = projects.filter(p => p.id === Number(id))
-
-    if (project) {
-        response.json(project)
-    } else {
-        response.status(404).end()
+projectSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        delete returnedObject.__v
+        delete returnedObject._id
     }
+})
+
+const Project = mongoose.model("Project", projectSchema)
+
+// get all projects
+app.get('/api/projects', (request, response) => {
+    Project.find({}).then(projects => {
+        response.json(projects)
+    })
+})
+
+// get individual project
+app.get('/api/projects/:id', (request, response) => {
+    Project.find({ id: request.params.id }).then(projects => {
+        response.json(projects)
+    })
 })
 
 // delete individual project
 app.delete('/api/projects/:id', (request, response) => {
-    const id = request.params.id
-    projects = projects.filter(p => p.id !== Number(id))
-    response.status(204).end()
+    Project.deleteOne({ id: request.params.id }).then(project => response.json(project))
 })
 
 // add individual project
 app.post('/api/projects', (request, response) => {
     // get body of post request
     const body = request.body
-    response.json(body)
 
-    const project = {
-        projectName: body.projectName,
-        id: body.id
+    if (body.projectName === undefined) {
+        return response.status(400).json({ error: "content missing" })
     }
 
-    projects = projects.concat(project)
-    //response.json(project)
+    const project = new Project({
+        projectName: body.projectName,
+        id: body.id
+    })
+
+    project.save().then(savedProject => {
+        response.json(savedProject)
+        console.log(body)
+    })
 })
 
 // delete and add individual tasks
 app.put('/api/projects/:id', (request, response) => {
-    const id = request.params.id
-    console.log(request.body)
-    let index = projects.findIndex(p => p.id === Number(id))
-    projects[index] = request.body
-    response.json(projects[index])
+    const body = request.body
+    const bodyArray = Object.entries(body)
+    const filterBody = bodyArray.filter(([key, value]) => key !== "id" && key !== "projectName")
+    //console.log(filterBody)
+    const toObj = Object.values(Object.fromEntries(filterBody))
+
+    if (bodyArray.length == 1) {
+        Project.findOneAndUpdate(
+            { id: request.params.id },
+            { $pull: { "projects": { task: toObj[0] } } }
+        ).then(task => {
+            response.json(task)
+        })
+    }
+
+    if (bodyArray.length != 1) {
+        Project.findOneAndUpdate(
+            { id: request.params.id },
+            { $addToSet: { "projects": { task: toObj[toObj.length - 1] } } }
+        ).then(task => {
+            response.json(task)
+        })
+    }
+
 })
 
 
